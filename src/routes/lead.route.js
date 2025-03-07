5384e5a0755ac365e2d16d89ebd1cbbeb13465d8
coolboyalan <deepaky954000@gmail.com>
2025-03-07 10:46:16 +0530

    removed auth from lead

--- src/routes/lead.route.js
+++ src/routes/lead.route.js
@@ -4,8 +4,6 @@ import { get, create, update, deleteDoc } from "#controllers/lead";
 
 const router = express.Router();
 
-router.use(authorization);
-
 router.route("/:id?").get(get).post(create).put(update).delete(deleteDoc);
 
 export default router;
